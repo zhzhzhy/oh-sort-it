68b3e5af6d7f2dbb79f4e793b32403c6b045abc7
Geek-cat <by6dx1@gmail.com>
2020-05-31 09:00:35 +0000

    Final bug fix:)

--- merge-sort.c
+++ merge-sort.c
@@ -9,12 +9,13 @@ void merge(int array[],int L,int R){
 	int right[right_size];
 	int i,j,k;
 	for(i = 0;i < left_size;i++){
-	   left[i] = array[i];
+	   left[i] = array[L + i];
 	}
 	for(i = 0;i < right_size;i++){
 	   right[i] = array[M + i + 1];
 	}
-	i = j = k = 0;
+	i = j = 0;
+	k= L;
 	while(i < left_size && j < right_size){
 		if(left[i] < right[j]){
 	   		array[k] = left[i];
@@ -55,7 +56,7 @@ void Mergesort(int array[],int L,int R){
 int main(){
 	int i;
 	int arr[] = {1,3,5,7,13,5,7,9,10};
-//	merge(arr,0,8);
+//	merge(arr,2,6);
 	Mergesort(arr,0,8);
 	for(i=0;i<=8;i++){
 		printf("%d\n",arr[i]);
