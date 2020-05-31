8b16f1c98019008f17eb19dd4d46f621963b4d50
Geek-cat <by6dx1@gmail.com>
2020-05-31 04:13:49 +0000

    edit mergesort

--- merge-sort.c
+++ merge-sort.c
@@ -1,15 +1,58 @@
 #include <stdio.h>
 #include <string.h>
 
-void merge(int array[],int M,int L,int R){
-	M = (L + R) / 2;
-	
-    
+void merge(int array[],int L,int R){
+	int M = (L + R) / 2;
+	int left_size = M - L + 1;
+	int right_size = R - M;
+	int left[left_size];
+	int right[right_size];
+	int i,j,k;
+	for(i = 0;i < left_size;i++){
+	   left[i] = array[i];
+	}
+	for(i = 0;i < right_size;i++){
+	   right[i] = array[M + i + 1];
+	}
+	i = j = k = 0;
+	while(i < left_size && j < right_size){
+		if(left[i] < right[i]){
+	   		array[k] = left[i];
+	   		i++;
+	   		k++;
+		}
+		else{
+			array[k] = right[j];
+			j++;
+			k++;
+		}
+	}
+	while(i < left_size){
+  		array[k] = left[i];
+   		i++;
+   		k++;
+	}
+	while(j < right_size){
+  		array[k] = left[j];
+   		j++;
+   		k++;
+	}
 
+	for(j=0;j<right_size;j++){
+	printf("%d\n",right[j]);
+	}
+	for(j=0;j<left_size;j++){
+	printf("%d\n",left[j]);
+	}
 
 }
 
 int main(){
-
-
+	int i;
+	int arr[] = {1,3,5,7,13,5,7,9,10};
+	merge(arr,0,8);
+	for(i=0;i<8;i++){
+		printf("%d\n",arr[i]);
+	}
+	return 0;
 }
