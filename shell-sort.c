bef73943368014c64bde713b2e21869e19f5bebf
Geek-cat <by6dx1@gmail.com>
2020-06-01 15:17:07 +0000

    shell sort

---
+++ shell-sort.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void shellsort(int *array,int len){
+	increment = len / 2;
+
+}
+int main(){
+	int array[] = {2,6,54,34,87,12,3,9,6,7};
+	len = sizeof(array)/sizeof(*array);
+
+}
